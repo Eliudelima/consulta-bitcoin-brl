b83e54f6781cdca437cebed2065779324a7fc079
Eliudelima <eliudeangelo@gmail.com>
2020-10-02 22:21:45 -0300

    4 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { GithubComponent } from './github/github.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
+import { BitcoinService } from './bitcoin/bitcoin.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+       
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'github', component: GithubComponent}, 
+      {path: 'bitcoin', component: BitcoinComponent}, 
+         
+            
+    ])
+  ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, GithubComponent, BitcoinComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [BitcoinService]
 })
 export class AppModule { }
